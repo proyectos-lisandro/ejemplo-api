4e65324d868f91992ce65aff48439c286701c262
lisandro <lmlisandro3@gmail.com>
2024-05-24 18:05:34 -0500

    en este commit ya funcionan 4 metodos los dos get el post y el delete en el proximos commit el put

--- src/controllers/controller.user.js
+++ src/controllers/controller.user.js
@@ -9,8 +9,14 @@ try {
     mensajes.error(req, res,500, error);
 }
 }
-const mostrarusuarios=()=>{
-
+const mostrarusuarios=async(req, res)=>{
+    const {id}= req.params;
+    try {
+        const respuesta = await pool.query(`CALL sp_mostrar_usuario(?);`,[id]);
+        mensajes.success(req, res, 200, respuesta[0]);
+    } catch (error) {
+        mensajes.error(req, res, 500, error);
+    }
 };
 const agregarusuarios= async(req, res)=>{
     const {nombre} =req.body;
@@ -29,8 +35,18 @@ const agregarusuarios= async(req, res)=>{
 const modificarusuarios=()=>{
 
 };
-const eliminarusuarios=()=>{
-
+const eliminarusuarios=async(req, res)=>{
+    const {id}= req.body;
+    try {
+        const respuesta = await pool.query(`CALL sp_eliminar_usuario(?);`,[id]);
+    if(respuesta[0].affectedRows==1){
+        mensajes.success(req, res, 200, "usuario eliminado");
+    }else{
+        mensajes.error(req, res, 400, "usuario no eliminado");
+    }
+    } catch (error) {
+     mensajes.error(req, res,500, error);
+    }
 };
 
 export const metodos= {
